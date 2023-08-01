028c0b5d2e5e346ffa598ab95e01fe7076470839
Likithsreenivasa <118590713+Likithsreenivasa@users.noreply.github.com>
2023-08-01 12:48:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
 //your JS code here. If required.
+    function resolveForm() {
+      const nameInput = document.getElementById("name");
+      const ageInput = document.getElementById("age");
+      const age = parseInt(ageInput.value);
+
+      if (!ageInput.value || !nameInput.value) {
+        alert("All fields are necessary to fill, please.");
+        return;
+      }
+
+      const promise = new Promise((resolve, reject) => {
+        setTimeout(() => {
+          if (age >= 18) {
+            resolve();
+          } else {
+            reject();
+          }
+        }, 3000);
+      });
+
+      promise
+        .then(() => {
+          alert(`Welcome, ${nameInput.value}. You can vote.`);
+          // You can perform additional actions on successful resolution here
+        })
+        .catch(() => {
+          alert(`Oh sorry ${nameInput.value}. You aren't old enough.`);
+          // You can perform additional actions on rejection here
+        });
+    }
+
+    const button = document.getElementById("btn");
+    button.addEventListener("click", resolveForm);
+
+
+
+
